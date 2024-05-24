ffb3415da5108e524c70fe9e68d51300e57f1f6f
victojoao <victojoao789@gmail.com>
2024-05-24 10:46:48 -0300

    feat: add class category method getter, setter, toString and equalsAndHashCode

--- src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Category.java
+++ src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +17,32 @@ public class Category implements Serializable {
     private UUID id;
     private String name;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
