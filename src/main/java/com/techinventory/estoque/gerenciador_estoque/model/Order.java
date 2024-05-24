50e661db37a89e5810a1f36f98289d5d10eb4a07
victojoao <victojoao789@gmail.com>
2024-05-24 10:57:57 -0300

    feat: create class order

---
+++ src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Order.java
@@ -0,0 +1,55 @@
+package com.techinventory.estoque.gerenciador_estoque.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+@Entity
+@Table(name = "TB_ORDER")
+
+public class Order implements Serializable {
+    private static final long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private LocalDateTime date;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", date=" + date +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
