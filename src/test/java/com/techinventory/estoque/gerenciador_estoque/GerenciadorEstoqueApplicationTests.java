64b28bcc22f0a76599feef073b83bca10f7ebd76
victojoao <victojoao789@gmail.com>
2024-05-24 10:24:58 -0300

    init project

---
+++ src/test/java/com/techinventory/estoque/gerenciador_estoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techinventory.estoque.gerenciador_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
