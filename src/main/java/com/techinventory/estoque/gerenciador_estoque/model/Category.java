70e9d95722b5acb2ef01439ced0b07f3c9adf227
jvictornascimento <victojoao789@gmail.com>
2024-05-26 12:03:31 -0300

    feat: impl. heteoas to Category class and service

--- src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Category.java
+++ src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Category.java
@@ -2,6 +2,7 @@ package com.techinventory.estoque.gerenciador_estoque.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_CATEGORY")
 
-public class Category implements Serializable {
+public class Category extends RepresentationModel<Category> implements Serializable {
     private static final long serialVersionUID=1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/techinventory/estoque/gerenciador_estoque/services/CategoryService.java
+++ src/main/java/com/techinventory/estoque/gerenciador_estoque/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.techinventory.estoque.gerenciador_estoque.services;
 
+import com.techinventory.estoque.gerenciador_estoque.controllers.CategoryController;
 import com.techinventory.estoque.gerenciador_estoque.dtos.category.CategoryDTO;
 import com.techinventory.estoque.gerenciador_estoque.model.Category;
 import com.techinventory.estoque.gerenciador_estoque.repositories.CategoryRepository;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Service
 
 public class CategoryService {
@@ -18,12 +22,19 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     public List<Category> findAll(){
-        return categoryRepository.findAll();
+        var categories = categoryRepository.findAll();
+        for (Category category : categories){
+            UUID id = category.getId();
+            category.add(linkTo(methodOn(CategoryController.class).getOneCategory(id)).withSelfRel());
+        }
+        return categories;
     }
 
     public Category findById(UUID id){
-        return categoryRepository.findById(id).
+        var category = categoryRepository.findById(id).
                 orElseThrow(CategoryNotFoundException :: new);
+        category.add((linkTo(methodOn(CategoryController.class).getAllCategories()).withRel("Categories List")));
+        return category ;
     }
 
     public Category save(CategoryDTO categoryDTO){
