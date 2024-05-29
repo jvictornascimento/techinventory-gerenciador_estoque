6433df7fc473c31d6b187d63f904aca647b7b859
jvictornascimento <victojoao789@gmail.com>
2024-05-28 21:14:10 -0300

    feat: refactor class client

--- src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Client.java
+++ src/main/java/com/techinventory/estoque/gerenciador_estoque/model/Client.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -25,4 +26,58 @@ public class Client implements Serializable {
     @JsonIgnore
     private List<Order> orders;
 
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(uuid, client.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "uuid=" + uuid +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", orders=" + orders +
+                '}';
+    }
 }
